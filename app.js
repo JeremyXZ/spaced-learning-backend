8f7c8c86b4a2b71490fe23ce803984000030acdb
JeremyXZ <jeremyuk168@gmail.com>
2023-05-03 11:22:16 +0100

    added allow cross-origin in App

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ import morgan from "morgan";
 import cors from "cors";
 import tasksRouter from "./routes/tasks.js";
 const app = express();
-const PORT = 4000;
+// const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
-app.use("/api/tasks", tasksRouter)
-
-
+app.use("/api/tasks", tasksRouter);
 
 export default app;
-
